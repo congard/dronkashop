906784dccf49d9621fcff3ba4dd6603661e1afcc
Daniel <dbcongard@gmail.com>
2023-05-08 10:50:48 +0200

    Add Session.runGraphQL

--- src/main/kotlin/pl/edu/agh/db2/dronkashop/backend/ext/SessionExt.kt
+++ src/main/kotlin/pl/edu/agh/db2/dronkashop/backend/ext/SessionExt.kt
@@ -3,6 +3,12 @@ package pl.edu.agh.db2.dronkashop.backend.ext
 import org.neo4j.driver.Result
 import org.neo4j.driver.Session
 import org.neo4j.graphql.Cypher
+import pl.edu.agh.db2.dronkashop.backend.provider.GraphQLProvider
+import pl.edu.agh.db2.dronkashop.framework.core.GraphQLQuery
+import pl.edu.agh.db2.dronkashop.framework.core.Params
 
 fun Session.run(cypher: Cypher): Result =
     this.run(cypher.query, cypher.params)
+
+fun Session.runGraphQL(query: GraphQLQuery, params: Params) =
+    run(GraphQLProvider.translate(query, params))
