fbe65e423d0191e419721fa9c267782e8814c366
Daniel <dbcongard@gmail.com>
2023-05-15 20:35:54 +0200

    Update Main

--- src/main/kotlin/pl/edu/agh/db2/dronkashop/backend/Main.kt
+++ src/main/kotlin/pl/edu/agh/db2/dronkashop/backend/Main.kt
@@ -21,24 +21,34 @@ fun start() {
             println(it)
             println(it.order.dst())
             println(it.item.dst())
+            it.quantity = 100
+            it.push()
+            println(it)
         }
     }
 
     val payment92: Payment = PaymentProvider.getById(ID(92)).also { println(it) }
     println(payment92.amount)
 
-    val role177: Role = RoleProvider.getById(ID(25)).also { println(it) }
-    role177.includes.forEach { userRelation -> println(userRelation.dst()) }
+    val role25: Role = RoleProvider.getById(ID(25)).also { println(it) }
+    role25.includes.forEach { userRelation -> println(userRelation.dst()) }
+
     val roleListClient: List<Role> = RoleProvider.getByName("Client").also { println(it) }
     println(roleListClient.first().description)
 
     val category23: Category = CategoryProvider.getById(ID(23)).also { println(it) }
     println(category23.name)
+//    category23.also {
+//        it.description = "Przekąski Test"
+//        it.includes.add(Relation.create(ID(174)))
+//        it.push()
+//    }
     val categoryListNapoje: List<Category> = CategoryProvider.getByName("Napoje").also { println(it) }
     println(categoryListNapoje.first().description)
 
     val user176: User = UserProvider.getById(ID(176)).also { println(it) }
     println(user176.displayName)
+
     val userListBiedronka: List<User> = UserProvider.getByName("Biedronka").also { println(it) }
     println(userListBiedronka.first().email)
 
