7a2893ca549a319dc18a09565ff561f0e09dfaee
Daniel <dbcongard@gmail.com>
2023-06-13 00:06:57 +0200

    [Backend] Providers: add ability to use custom runner

--- src/main/kotlin/pl/edu/agh/db2/dronkashop/backend/provider/PaymentProvider.kt
+++ src/main/kotlin/pl/edu/agh/db2/dronkashop/backend/provider/PaymentProvider.kt
@@ -2,7 +2,9 @@ package pl.edu.agh.db2.dronkashop.backend.provider
 
 import pl.edu.agh.db2.dronkashop.backend.entity.Payment
 import pl.edu.agh.db2.dronkashop.framework.core.ID
+import pl.edu.agh.db2.dronkashop.framework.provider.DBProvider
 import pl.edu.agh.db2.dronkashop.framework.provider.EntityProvider
+import pl.edu.agh.db2.dronkashop.framework.runner.QueryRunner
 
 object PaymentProvider {
 
@@ -10,6 +12,6 @@ object PaymentProvider {
      * If Payment with the specified id hasn't been loaded yet,
      * it will be loaded, otherwise cached object will be returned
      */
-    fun getById(id: ID): Payment =
-        EntityProvider.entityById(id)
+    fun getById(id: ID, runner: QueryRunner = DBProvider.defaultQueryRunner): Payment =
+        EntityProvider.entityById(id, runner = runner)
 }
